85e8571741a323472e1b541fee4d3b51b21292cf
Patrick <nyatindopatrick@gmail.com>
2020-08-04 13:47:06 +0300

    add projects class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import '../styles/style.scss';
+import Projects from './projects';
+
+const projects = new Projects();
+
+// console.log(projects.addProject('library'));
+console.log(projects.deleteProject('library'));
+console.log(projects.getProjects());
+
+// const proJect = [
+//   {
+//     prjectName: 'library',
+//     tasks: [
+//       {
+//         taskName: 'cleaning',
+//         date: new Date(),
+//       },
+//     ],
+//   },
+// ];
