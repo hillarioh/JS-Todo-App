c46590568fce543d47f6a783ef77a836e009e922
Patrick <nyatindopatrick@gmail.com>
2020-08-04 12:44:15 +0300

    add sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
 const path = require('path');
 
 module.exports = {
-  entry: './js/index.js',
+  mode: 'development',
+  entry: path.join(__dirname, 'src', 'index.js'),
+  watch: true,
   output: {
-    filename: 'main.js',
     path: path.join(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: 'main.js',
+    chunkFilename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(scss|sass|css)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.json', '.js'],
   },
 };
