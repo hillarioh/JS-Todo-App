980897296ddfe33776f96c20e70071312e64140f
hillarioh <okerioh@gmail.com>
2020-08-05 06:07:27 +0300

    add task modal

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import Projects from './projects';
 
 const showProject = document.getElementById('show-project');
 const projectModal = document.getElementById('project-modal');
+const taskModal = document.getElementById('task-modal');
 const close = document.getElementById('close');
 const projectForm = document.getElementById('project-form');
 const name = document.getElementById('name');
 const projectsList = document.getElementById('projects-list');
+const newTask = document.getElementById('new-task');
 
 const projects = new Projects();
 
@@ -43,9 +45,15 @@ showProject.addEventListener('click', () => {
   projectModal.classList.add('show-modal');
 });
 
+newTask.addEventListener('click',()=>{
+  taskModal.classList.add('show-modal');
+})
+
 close.addEventListener('click', () => {
   if (projectModal.classList.contains('show-modal')) {
     projectModal.classList.remove('show-modal');
+  } else {
+    taskModal.classList.remove('show-modal');
   }
 });
 
